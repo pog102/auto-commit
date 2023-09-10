db10b75a1df086dc5eb6ee5ed9d0186de15dba80
Your Name <you@example.com>
2023-09-10 20:22:26 +0000

    autp

--- auto.sh
+++ auto.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+x=$(shuf -i 0-7 -n 1)
 pro="$HOME/auto-commit"
+for i in $(seq 1 $x);
+do
 ts=$(date +%s)
 echo "$ts" > $pro/main.txt
 git -C $pro add .
 git -C $pro commit -m "autp"
-git -C $pro push 
+git -C $pro push
+done
